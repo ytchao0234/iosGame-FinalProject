f6cfd387b67ee08ff1445843ef2da7c8e813a60c
ytchao0234 <nd68319@gmail.com>
2022-05-12 18:41:58 +0800

    Initial Commit

---
+++ kittyPainter/kittyPainterApp.swift
@@ -0,0 +1,17 @@
+//
+//  kittyPainterApp.swift
+//  kittyPainter
+//
+//  Created by FanRende on 2022/5/12.
+//
+
+import SwiftUI
+
+@main
+struct kittyPainterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
