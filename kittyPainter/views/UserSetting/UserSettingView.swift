0cadad2b88940bf658ebf4d96ed5760b28489f70
ytchao0234 <nd68319@gmail.com>
2022-06-09 23:35:50 +0800

    fix: keyboard focused in UserSettingView
    Signed-off-by: ytchao0234 <nd68319@gmail.com>

--- kittyPainter/views/UserSetting/UserSettingView.swift
+++ kittyPainter/views/UserSetting/UserSettingView.swift
@@ -10,65 +10,65 @@ import SwiftUI
 struct UserSettingView: View {
     @EnvironmentObject var auth: UserViewModel
     @Binding var userDetail: UserDetail
+    @FocusState var focus: Bool
 
     var body: some View {
-        ZStack(alignment: .bottom) {
-            VStack {
-                ZStack(alignment: .bottomTrailing) {
-                    HeadshotView(userDetail: userDetail, size: 200)
-                    
-                    Button {
-                        userDetail.headshot = Headshot.random()
-                    } label: {
-                        Image(systemName: "dice.fill")
-                            .resizable()
-                            .scaledToFit()
-                            .padding(5)
-                            .frame(width: 40)
-                    }
-                    .modifier(ButtonViewModifier(shape: Circle(), background: .blue, toStroke: false))
-                }
-                .frame(width: 200, height: 200)
+        VStack {
+            ZStack(alignment: .bottomTrailing) {
+                HeadshotView(userDetail: userDetail, size: 200)
                 
-                List {
-                    Section {
-                        Group {
-                            TextField(text: $userDetail.name, prompt: Text(userDetail.name)) {}
-                                .modifier(TextFieldViewModifier(image: "person.circle.fill"))
-                            HeadshotSettingView(headshot: $userDetail.headshot)
-                        }
-                        .padding(.vertical, 5)
-                    } header: {
-                        Label("Modifiable Information", systemImage: "pencil.and.outline")
-                            .font(.caption)
-                    }
-                    Section {
-                        Group {
-                            FixedInformation(title: "Email", content: auth.user.email)
-                            FixedInformation(title: "Joined Time", content: auth.user.detail.joinTime.formatted(.dateTime))
-                            FixedInformation(title: "Accuracy Rate", content: "\(auth.user.detail.accuracyRate)%")
-                            HStack {
-                                Text("Hints")
-                                Spacer()
-                                ForEach(0..<UserDetail.maxHints) { idx in
-                                    Image(systemName: "lightbulb.fill")
-                                        .foregroundColor((idx < auth.user.detail.hints) ? .yellow : .secondary)
-                                }
-                                Image(systemName: "play.rectangle")
-                                    .foregroundColor(.blue)
-                                    .padding(.leading, 5)
-                                    .onTapGesture {
-                                        print("Ad")
-                                    }
-                            }
-                        }
-                        .padding(.vertical, 5)
-                    } header: {
-                        Label("Fixed Information", systemImage: "info.circle")
-                            .font(.caption)
-                    }
-
+                Button {
+                    userDetail.headshot = Headshot.random()
+                } label: {
+                    Image(systemName: "dice.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .padding(5)
+                        .frame(width: 40)
                 }
+                .modifier(ButtonViewModifier(shape: Circle(), background: .blue, toStroke: false))
+            }
+            .frame(width: 200, height: 200)
+            
+            Form {
+                Section {
+                    Group {
+                        TextField(text: $userDetail.name, prompt: Text(userDetail.name)) {}
+                            .modifier(TextFieldViewModifier(image: "person.circle.fill"))
+                            .focused($focus)
+                        HeadshotSettingView(headshot: $userDetail.headshot)
+                    }
+                    .padding(.vertical, 5)
+                } header: {
+                    Label("Modifiable Information", systemImage: "pencil.and.outline")
+                        .font(.caption)
+                }
+                Section {
+                    Group {
+                        FixedInformation(title: "Email", content: auth.user.email)
+                        FixedInformation(title: "Joined Time", content: auth.user.detail.joinTime.formatted(.dateTime))
+                        FixedInformation(title: "Accuracy Rate", content: "\(auth.user.detail.accuracyRate)%")
+                        HStack {
+                            Text("Hints")
+                            Spacer()
+                            ForEach(0..<UserDetail.maxHints) { idx in
+                                Image(systemName: "lightbulb.fill")
+                                    .foregroundColor((idx < auth.user.detail.hints) ? .yellow : .secondary)
+                            }
+                            Image(systemName: "play.rectangle")
+                                .foregroundColor(.blue)
+                                .padding(.leading, 5)
+                                .onTapGesture {
+                                    print("Ad")
+                                }
+                        }
+                    }
+                    .padding(.vertical, 5)
+                } header: {
+                    Label("Fixed Information", systemImage: "info.circle")
+                        .font(.caption)
+                }
+
             }
         }
         .onAppear {
@@ -77,6 +77,14 @@ struct UserSettingView: View {
         .onChange(of: auth.user.detail.uid) { newValue in
             self.userDetail = auth.user.detail
         }
+        .background(
+            Color.clear
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    focus = false
+                }
+        )
     }
 }
 
